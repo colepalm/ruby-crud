c14557fe5485ebdf646688aa4801fee669a2fe3a
Cole Palm <palm.cole@gmail.com>
2016-10-06 13:42:37 -0700

    react

---
+++ app/assets/javascripts/components/_header.js.jsx
@@ -0,0 +1,9 @@
+var Header = React.createClass({
+    render() {
+        return (
+            <div>
+                <h1>Hello, you fucking kike</h1>
+            </div>
+        )
+    }
+})
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  namespace :api do
-    namespace :v1 do
-      resources :items, only: [:index, :create, :destroy, :update]
-    end
-  end
+  root to: 'site#index'
 end
